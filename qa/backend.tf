b069bcfd586c8dbadf61ad47e564ecd81532309d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-07 09:39:59 +0530

    Update backend.tf

--- qa/backend.tf
+++ qa/backend.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0.0"  # Ensures Terraform version compatibility
+
   backend "azurerm" {
     resource_group_name  = "backend-rg"
     storage_account_name = "backendstorage"
